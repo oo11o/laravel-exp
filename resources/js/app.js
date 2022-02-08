ed5cdf3701636a10e8a661ee9523bb8dcedd0d38
Vlad Imir <ymyr.vlad@gmail.com>
2022-02-08 17:38:45 +0200

    add vue component PostIndex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+import { createApp } from 'vue';
+
+import PostIndex from './components/Posts/Index';
+
+const app = createApp({
+    components: {
+        PostIndex
+    }
+})
+
+app.mount('#app');
---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Some Name</th>
+                    <th>Post Text</th>
+                    <th>Create Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Some</th>
+                    <th>Some</th>
+                    <th>Some</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+</script>
