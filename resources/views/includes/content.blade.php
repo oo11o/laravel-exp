c55c6ed2a73b17e2f2c565106169621436006245
oo11o <ymyr.vlad@gmail.com>
2022-02-13 23:37:47 +0200

    refactoring blade laravel

---
+++ resources/views/includes/content.blade.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div id="app">
+        <app></app>
+    </div>
+</div>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.index')
+
+@section('content')
+    @include('includes.content')
+@endsection
